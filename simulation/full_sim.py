750f1a34fb26806886a007c152f579d3640aa790
Austin Xu <austinxu87@gmail.com>
2024-05-08 15:57:54 -0400

    cheat feasibility

--- simulation/full_sim.py
+++ simulation/full_sim.py
@@ -8,7 +8,7 @@ import random
 from collections import defaultdict
 
 np.random.seed(2024)
-solvers = [cp.SCS, cp.CVXOPT, cp.MOSEK]
+solvers = [cp.SCS, cp.CVXOPT]
 
 # forward model
 def generate_gt(n_ref: int, eigenvalue_gap: int) -> dict:
@@ -90,15 +90,18 @@ def generate_measurements(num_meas: int, gt: dict, query_type: str = 'paq', thre
         responses_n = []
         feat_matrix_n = np.zeros((2, 2, num_meas))
         Sig_n = gt['sigma_stars'][n]
-        if query_type == 'paq':      
+        if 'paq' in query_type:      
             for i in range(num_meas):
                 a_i = np.random.normal(size=(2))
                 quad_i = a_i.T @ Sig_n @ a_i
-                gamma_i = thresh / quad_i
+                if 'noisy' in query_type:
+                    gamma_i = (thresh + np.random.uniform(low=-thresh, high=thresh)) / quad_i
+                else:
+                    gamma_i = thresh / quad_i
 
                 feat_matrix_n[:,:,i] = gamma_i * np.outer(a_i, a_i)
                 responses_n.append( gamma_i )
-
+        
         elif query_type == 'triplet':
             for i in range(num_meas):
                 delta_i = np.random.normal(size=(2,2))
@@ -143,7 +146,7 @@ def estimate_metric(responses: dict, thresh: float = 1, query_type: str = 'paq',
         # estimation
         Sig_hat = cp.Variable((2,2), PSD=True)
 
-        if query_type == 'paq':
+        if 'paq' in query_type:
             loss = cp.sum_squares( thresh - (cp.vec(Sig_hat) @ feat_flat_n) ) / num_meas #+ 0.1 * cp.norm(Sig_hat, 'fro')
 
         elif query_type == 'triplet':
@@ -162,7 +165,7 @@ def estimate_metric(responses: dict, thresh: float = 1, query_type: str = 'paq',
             prob.solve(solver=solvers[ct], max_iters = max_iter)
             ct += 1
         
-        print(f'Tried {ct + 1} solvers, status {prob.status}')
+        #print(f'Tried {ct + 1} solvers, status {prob.status}')
         
         est = Sig_hat.value
         est_out[n] = est
@@ -171,7 +174,7 @@ def estimate_metric(responses: dict, thresh: float = 1, query_type: str = 'paq',
 
 
 # step 2: compute cones
-def compute_cones(est_out: dict, gt: dict, num_meas: int, cheat: bool = True, const: float = 10) -> tuple[list, list]:
+def compute_cones(est_out: dict, gt: dict, num_meas: int, cheat: bool = False, const: float = 10) -> tuple[list, list]:
     alphas = []
     major_axes = []
 
@@ -196,8 +199,19 @@ def compute_cones(est_out: dict, gt: dict, num_meas: int, cheat: bool = True, co
 
     return alphas, major_axes
 
+def feas_check(w_star: np.ndarray, zn: np.ndarray, u2_hat: np.ndarray, alpha: float) -> bool:
+    u21_hat, u22_hat = u2_hat[0], u2_hat[1]
+
+    cos_alpha_2 = np.cos(alpha / 2)
+    sin_alpha_2 = np.sin(alpha / 2)
+
+    cond_one = (w_star[1] - zn[1]) * (cos_alpha_2 * u21_hat - sin_alpha_2 * u22_hat) <= (w_star[0] - zn[0]) * (sin_alpha_2 * u21_hat + cos_alpha_2 * u22_hat)
+    cond_two = (w_star[1] - zn[1]) * (cos_alpha_2 * u21_hat + sin_alpha_2 * u22_hat) >= (w_star[0] - zn[0]) * (-u21_hat * sin_alpha_2 + u22_hat * cos_alpha_2)
+
+    return (cond_one and cond_two)
+
 # step 3: Estimate copunct
-def estimate_copunctal(alphas: dict, major_axes: dict, refs: list, w_star: np.ndarray) -> np.ndarray:
+def estimate_copunctal(alphas: dict, major_axes: dict, refs: list, w_star: np.ndarray = None) -> tuple[np.ndarray, str]:
     w = cp.Variable(2) # Decision variable for the copunctal point w
     constraints = []
     # Add constraints for each reference point and its corresponding cone
@@ -205,47 +219,56 @@ def estimate_copunctal(alphas: dict, major_axes: dict, refs: list, w_star: np.nd
         # Unpack the eigenvalue vector for readability
         u21_hat, u22_hat = u2_hat[0], u2_hat[1]
 
-        if np.sign(w_star[0] - zn[0]) != np.sign(u21_hat) or np.sign(w_star[1] - zn[1]) != np.sign(u22_hat):
-            continue
-
         cos_alpha_2 = np.cos(alpha / 2)
         sin_alpha_2 = np.sin(alpha / 2)
+
+        if w_star is not None and not feas_check(w_star, zn, u2_hat, alpha):
+            continue
 
         # Upper side of the cone
         constraints.append((w[1] - zn[1]) * (cos_alpha_2 * u21_hat - sin_alpha_2 * u22_hat) <= (w[0] - zn[0]) * (sin_alpha_2 * u21_hat + cos_alpha_2 * u22_hat))
         # Lower side of the cone
         constraints.append((w[1] - zn[1]) * (cos_alpha_2 * u21_hat + sin_alpha_2 * u22_hat) >= (w[0] - zn[0]) * (-u21_hat * sin_alpha_2 + u22_hat * cos_alpha_2))
-
     objective = cp.Minimize(0)
 
     problem = cp.Problem(objective, constraints)
-    problem.solve(solver=cp.ECOS) 
-    # ? Try CVSOPT, SCS and ECOS as solver. 
-    # ECOS performs better in num_refs experiments, but it fails in threshold experiment
-    # CVSOPT is better in threshold experiment, but it fails in eigenvalue gap experiment
-    # SCS can run all experiments
+    problem.solve(solver=cp.SCS)
+    ct = 0
+    while problem.status != cp.OPTIMAL and ct < len(solvers):
+        problem.solve(solver=solvers[ct])
+        ct += 1
 
-    # Print the solution
-    # print("The estimated copunctal point w is:", w.value)
-    # print("The true copunctal point w is:", w_star)
-    return w.value
+    return w.value, problem.status
+
+
+MAX_RETRIES = 50
+TRIALS = 20
 
 num_ref = 25
 eigenvalue_gap = 10
 query_type = 'triplet'
-num_meas = 75
+num_meas = 3
 thresh = 5
-const = 1000
+const_start = 10
 
-gt_out = generate_gt(num_ref, eigenvalue_gap)
-meas_out = generate_measurements(num_meas, gt_out, query_type=query_type, thresh=thresh)
-est_out = estimate_metric(meas_out, query_type=query_type, thresh=thresh)
-angles_out, major_axes_out = compute_cones(est_out, gt_out, num_meas, const=const)
+for mc in range(TRIALS):
+    gt_out = generate_gt(num_ref, eigenvalue_gap)
+    meas_out = generate_measurements(num_meas, gt_out, query_type=query_type, thresh=thresh)
+    est_out = estimate_metric(meas_out, query_type=query_type, thresh=thresh)
 
-w_star = gt_out['w_star']
-w_hat = estimate_copunctal(angles_out, major_axes_out, gt_out['refs'], w_star)
+    const = const_start
+    w_star = gt_out['w_star']
+    angles_out, major_axes_out = compute_cones(est_out, gt_out, num_meas, const=const)
+    w_hat, status = estimate_copunctal(angles_out, major_axes_out, gt_out['refs'], w_star=w_star)
 
-print(f'w_hat: {w_hat} | w_star: {w_star}')
-err = np.linalg.norm(w_hat - w_star)**2
+    ct = 0
+    while status != cp.OPTIMAL and ct < MAX_RETRIES:
+        const += 10
+        angles_out, major_axes_out = compute_cones(est_out, gt_out, num_meas, const=const)
+        w_hat, status = estimate_copunctal(angles_out, major_axes_out, gt_out['refs'], w_star = w_star)
+        ct += 1
+    print(const)
 
-print(f'sq err: {err}')
+    
+    print(f'w_hat: {w_hat} | status: {status}')
+    print('-------------')
